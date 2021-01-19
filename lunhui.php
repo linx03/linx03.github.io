6af62943556c52b4ad64e10306add9ca56f11d31
linx03 <60433216+linx03@users.noreply.github.com>
2021-01-19 19:35:39 +0800

    Update lunhui.php

--- lunhui.php
+++ lunhui.php
@@ -6,7 +6,7 @@
 <meta name="keywords" content="小林早睡,小北IT,玩机,灰度,灰度开源项目,软件库,拾穗集,还你干净的世界" />
 <meta name="description" content="小林早睡,小北IT，轻奢，极简，悦享，还你干净的世界" />
 <title>小林-早睡</title>
-<link rel="shortcut icon" href="https://p.ananas.chaoxing.com/star3/origin/25d738210211b8ff86ddc084e586d221.png?rw=450&rh=450&_fileSize=327636&_orientation=1" />
+<link rel="shortcut icon" href="https://p.ananas.chaoxing.com/star3/origin/25d738210211b8ff86ddc084e586d221.png?rw=450rh=450_fileSize=327636_orientation=1" />
 <link rel="stylesheet" href="https://i.xiaobeiit.cn/css/clock.css">
 </head>
 <body>
