b79871306b3f5f7700dfe4cc391215778c6356d9
linx03 <60433216+linx03@users.noreply.github.com>
2021-01-19 19:32:52 +0800

    Update and rename wps.php to lunhui.php

---
+++ lunhui.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+<meta name="keywords" content="小林早睡,小北IT,玩机,灰度,灰度开源项目,软件库,拾穗集,还你干净的世界" />
+<meta name="description" content="小林早睡,小北IT，轻奢，极简，悦享，还你干净的世界" />
+<title>小林-早睡</title>
+<link rel="shortcut icon" href="https://p.ananas.chaoxing.com/star3/origin/25d738210211b8ff86ddc084e586d221.png?rw=450&rh=450&_fileSize=327636&_orientation=1" />
+<link rel="stylesheet" href="https://i.xiaobeiit.cn/css/clock.css">
+</head>
+<body>
+<ul class="clock" id="helang-clock">
+<hr>
+</ul>
+<script type="text/javascript" src="https://i.xiaobeiit.cn/js/jquery.min.js"></script>
+<script type="text/javascript" src="https://i.xiaobeiit.cn/js/clock.js"></script>
+<script type="text/javascript">
+        $("#helang-clock").clock();
+    </script>
+</body>
+</html>
